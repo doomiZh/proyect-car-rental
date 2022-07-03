62d1b856e50dfb002f1fafae16a8570a8b9743ef
Luis Bardales <lbardales@ekkomedia.com>
2022-07-03 07:22:59 -0700

    transaccion

--- src/main/java/cibertec/proyecto/controllers/VehiculoModeloApiController.java
+++ src/main/java/cibertec/proyecto/controllers/VehiculoModeloApiController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +38,7 @@ public class VehiculoModeloApiController {
 	}
 	
 	@PostMapping
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public ResponseEntity<VehiculoModelo> crear(VehiculoModelo vehiculoModelo) {
 		VehiculoModelo vehiculoModeloGuard = vehiculoModeloService.save(vehiculoModelo);
 
@@ -46,6 +49,7 @@ public class VehiculoModeloApiController {
 	}
 	
 	@PostMapping("{id}")
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public ResponseEntity<VehiculoModelo> editar(@PathVariable int id, VehiculoModelo vehiculoModelo) {
 		VehiculoModelo vehiculoModeloGuard = vehiculoModeloService.update(id, vehiculoModelo);
 
